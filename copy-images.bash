c14fbd7fb5688db740ba1c058ec9abe7e90bf12f
Tim Hoolihan <tim@hoolihan.net>
2026-03-03 08:36:40 -0500

    Setup Chapter 2 for cavern, move bunner to 3

--- copy-images.bash
+++ copy-images.bash
@@ -4,7 +4,8 @@ declare -A dirs
 
 br="book"
 dirs["ch01-boing"]="${br}/boing-master"
-dirs["ch02-bunner"]="${br}/bunner-master"
+dirs["ch02-cavern"]="${br}/cavern-master"
+dirs["ch03-bunner"]="${br}/bunner-master"
 
 for key in ${!dirs[@]}; do
     src=${dirs[${key}]}
--- copy-images.bat
+++ copy-images.bat
@@ -5,6 +5,11 @@ set br=book
 xcopy /e /i /y "%br%\boing-master\images" "ch01-boing\images\"
 xcopy /e /i /y "%br%\boing-master\music" "ch01-boing\music\"
 xcopy /e /i /y "%br%\boing-master\sounds" "ch01-boing\sounds\"
-xcopy /e /i /y "%br%\bunner-master\images" "ch02-bunner\images\"
-xcopy /e /i /y "%br%\bunner-master\music" "ch02-bunner\music\"
-xcopy /e /i /y "%br%\bunner-master\sounds" "ch02-bunner\sounds\"
+
+xcopy /e /i /y "%br%\cavern-master\images" "ch02-cavern\images\"
+xcopy /e /i /y "%br%\cavern-master\music" "ch02-cavern\music\"
+xcopy /e /i /y "%br%\cavern-master\sounds" "ch02-cavern\sounds\"
+
+xcopy /e /i /y "%br%\bunner-master\images" "ch03-bunner\images\"
+xcopy /e /i /y "%br%\bunner-master\music" "ch03-bunner\music\"
+xcopy /e /i /y "%br%\bunner-master\sounds" "ch03-bunner\sounds\"
